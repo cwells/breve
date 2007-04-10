ece14b551742f14f741aa55007fd1c7eeb174973
Cliff Wells <cliff@develix.com>
2007-04-10 10:00:31 +0000

    debugging TG issues

--- breve/template.py
+++ breve/template.py
@@ -143,7 +143,7 @@ class Template ( object ):
         else:
             _g.update ( T.vars )
 
-        print "GEE", T.vars.items()
+        print "NAMESPACE VARS", T.vars[ns].items()
         try:
             bytecode = _cache.compile ( filename, T.root, T.loaders [ -1 ] )
             output = flatten ( eval ( bytecode, T.tags, T.vars ) )
