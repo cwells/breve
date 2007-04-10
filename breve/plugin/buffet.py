07213f3752ac62026708ee5a6590dffeff480090
Cliff Wells <cliff@develix.com>
2007-04-10 01:53:45 +0000

    debugging TG issues

--- breve/plugin/buffet.py
+++ breve/plugin/buffet.py
@@ -38,7 +38,7 @@ class BreveTemplatePlugin ( object ):
 
         if 'std' in vars and 'tg' in vars: # turbogears-specific hacks
             cfg = vars [ 'std'] [ 'config' ]
-            print cfg
+            print "TG CONFIG", vars [ 'std' ]
             breve_opts [ 'root' ] = cfg ( 'breve.root', breve_opts [ 'root' ] )
             breve_opts [ 'namespace' ] = cfg ( 'breve.namespace', breve_opts [ 'namespace' ] )
             breve_opts [ 'debug' ] = cfg ( 'breve.debug', breve_opts [ 'debug' ] )
