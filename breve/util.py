2b1b99046cb5a64fd257e67e709faaeefdc0354a
Cliff Wells <cliff@develix.com>
2007-04-10 11:04:20 +0000

    debugging TG issues

--- breve/util.py
+++ breve/util.py
@@ -21,7 +21,11 @@ class Namespace ( object ):
         
     def __getattr__ ( self, k ):
         print "DEBUG", k, self._dict.keys ( ), k in self._dict
-        return self._dict [ k ]
+        try:
+            return self._dict [ k ]
+        except KeyError:
+            return getattr ( self._dict, k )
+        
         return self._dict.get ( k, getattr ( self._dict, k ) )
 
 def quoteattrs ( attrs ):
