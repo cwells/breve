45bc593842c971f99178e8f3bc030ab2597e060b
Cliff Wells <cliff@develix.com>
2007-04-11 22:35:32 +0000

    add check to override tag to see if already defined

--- breve/template.py
+++ breve/template.py
@@ -84,9 +84,10 @@ class Template ( object ):
     def override ( T, name ):
         class override ( Tag ): 
             def __str__ ( self ):
-                # if self.name in T.fragments:
-                #    print self.name, "ALREADY IN FRAGMENTS"
-                #    return u''
+                if self.name in T.fragments:
+                    print self.name, "ALREADY IN FRAGMENTS"
+                    self.children = T.fragments [ self.name ]
+                
                 if self.children:
                     return ( u''.join ( [ flatten ( c ) for c in self.children ] ) )
                 return u''
