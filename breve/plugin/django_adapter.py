36c5731dced391525f2c772a986ee58f978e5fe1
Cliff Wells <cliff@develix.com>
2007-04-28 00:09:48 +0000

    fixes for Django adapter

--- breve/plugin/django_adapter.py
+++ breve/plugin/django_adapter.py
@@ -1,34 +1,80 @@
+import os
 from breve import Template
 from breve.tags import html
 from django.http import HttpResponse
+from django.template import Context, TemplateDoesNotExist
+from django.utils.translation import gettext_lazy as _
 
 from django.conf import settings 
 BREVE_ROOT = settings.BREVE_ROOT
 
-def flatten_string(obj):
-    return unicode(obj).encode(settings.DEFAULT_CHARSET)
+def flatten_string ( obj ):
+    return unicode ( obj ).encode ( settings.DEFAULT_CHARSET )
 
 class _loader ( object ):
     def __init__ ( self, root, breve_opts = None ):
         self.breve_opts = breve_opts or { }
-        self.template_root = root
+        self.root = root
 
     def get_template ( self, template ):
-        self.template_filename = template
-        self.template_obj = Template ( tags = html.tags, root = self.template_root )
-        return self
-    
+        return TemplateAdapter ( [ template ], root = self.root,
+                                 breve_opts = self.breve_opts )
+
+    def select_template ( self, template_list ):
+        return TemplateAdapter ( template_list, root = self.root,
+                                 breve_opts = self.breve_opts )
+
+class TemplateAdapter ( object ):
+    """
+    Takes the root and a list of filenames of a breve templates in the
+    constructor and returns an object that behaves as a
+    django.templates.Template in the sense that the render (vars)
+    method returns the rendered string. It tries the filenames in
+    order and the first one for which a corresponding file exists is
+    used. If none of the filenames matches, render raises
+    django.template.TemplateDoesNotExist.
+    """
+    def __init__ ( self, names, root = BREVE_ROOT, breve_opts = { } ):
+        self.template = Template ( tags = html.tags, root = root )
+        self.names = names
+        self.breve_opts = breve_opts
+
     def render ( self, vars = None ):
-        self.vars = vars or { }
-        return flatten_string(self.template_obj.render ( template = self.template_filename,
-                                          vars = vars, **self.breve_opts ))
+        if vars == None:
+            vars = { }
+        elif isinstance ( vars, Context ):
+            result = { }
+            for d in [ d for d in vars ].__reversed__ ( ):
+                result.update ( d )
+            vars = result
+        for name in self.names:
+            try:                
+                return flatten_string (
+                    self.template.render (
+                        template = name, vars = vars,
+                        **self.breve_opts )
+                )
+            except OSError:
+                pass
+        if len ( self.names ) == 0:
+            raise TemplateDoesNotExist ( _( "No templates given." ) )
+        elif len ( self.names ) == 1:
+            raise TemplateDoesNotExist (
+                _( "The template %s does not exist." ) %
+                os.path.join ( self.template.root, self.names [ 0 ] ) )
+        else:
+            import os
+            raise TemplateDoesNotExist ( _( "None of the following templates exists: %s." ) %
+                                         ", ".join ( [ os.path.join (self.template.root, name )
+                                                       for name in self.names ] ) )
+
 
 loader = _loader ( root = BREVE_ROOT )
 
 def render_to_response ( template, vars = None ):
     t = loader.get_template ( template )
-    return HttpResponse ( flatten_string(t.render ( vars )))
+    return HttpResponse ( flatten_string ( t.render ( vars ) ) )
 
 def render_to_string ( template, vars = None ):
     t = loader.get_template ( template )
-    return flatten_string(t.render ( vars ))
+    return flatten_string ( t.render ( vars ) )
