d9bcf55d8fb3544811191599e79c1e10b961eba2
Cliff Wells <cliff@develix.com>
2008-07-24 02:15:41 +0000

    added simple adapter for Pylons 0.9.7 and greater

---
+++ breve/plugin/pylons_adapter.py
@@ -0,0 +1,23 @@
+'''
+Simple adapter for Pylons 0.9.7 and greater.  
+Earlier versions of Pylons should use the Buffet adapter.
+'''
+
+from pylons.templating import pylons_globals
+from breve import Template
+from breve.tags.html import tags
+
+def render ( template_name, tmpl_vars = None ):
+    if tmpl_vars is None:
+        tmpl_vars = { }
+
+    g = pylons_globals ( )
+    tmpl_vars.update ( g )
+
+    try:
+        opts = g [ 'app_globals' ].breve_opts
+    except AttributeError:
+        opts = { }
+
+    t = Template ( tags, **opts )
+    return t.render ( template_name, vars = tmpl_vars )
