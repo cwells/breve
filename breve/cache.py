8315b2f638fc9157b438ed705b45cf95ab9f4968
Sven Berkvens-Matthijsse <sven@csaw.nl>
2010-10-20 20:32:00 +0200

    Do not blindly put brackets around a file, but skip lines at the top that start with a comment so that one may embed encoding comments in a Breve template file.

--- breve/cache.py
+++ breve/cache.py
@@ -11,9 +11,30 @@ class Cache ( object ):
         if uid in self.ccache:
             if timestamp == self.ccache [ uid ][ 'timestamp' ]:
                 return self.ccache [ uid ][ 'bytecode' ]
+        to_compile = loader.load(uid)
+        if to_compile[:1] == "#":
+            # Skip lines that start with a comment, so that for example
+            # encoding information is left intact. Putting a newline after
+            # the opening bracket is also an option, but that makes the
+            # line numbers in error messages off-by-one.
+            i = -1
+            while i < len(to_compile):
+                i = to_compile.find("\n", i + 1)
+                if i == -1:
+                    break
+                if to_compile[i + 1 : i + 2] != "#":
+                    break
+            if i >= 0:
+                to_compile = to_compile[:i + 1] + \
+                        "(" + to_compile[i + 1:] + "\n)"
+        else:
+            # The code starts immediately, no encoding information. So simply
+            # put the container brackets in front and at the end. This will
+            # not change the line number information in error messages.
+            to_compile = "(" + to_compile + "\n)"
         self.ccache [ uid ] = dict (
             timestamp = timestamp,
-            bytecode = compile ( '(' + loader.load ( uid ) + ')', template, 'eval' )
+            bytecode = compile ( to_compile, template, 'eval' )
         )
         return self.ccache [ uid ] [ 'bytecode' ]
 
